672239c62325e762a7cf0a607d788448503caeec
Tianze Jiang <tianze.jiang@citrix.com>
2020-01-15 16:14:49 +0800

    Use ng material to design navbar

--- src/app/modules/b-club-material/b-club-material.module.ts
+++ src/app/modules/b-club-material/b-club-material.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatDividerModule } from '@angular/material/divider'
+import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @NgModule({
   imports: [
     CommonModule,
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   exports: [
     MatDividerModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   declarations: []
 })
